84f760a9a1f040a9a14ac68b263ebe278fa636ba
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-02-24 11:49:18 -0700

    Added auth

---
+++ auth.js
@@ -0,0 +1,20 @@
+// Load required packages
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+        //hard coded
+        var user = { name: "admin" };
+        if (username === user.name && password === "password")
+        {
+            return done(null, user);
+        }
+        else
+        {
+            return done(null, false);
+        }
+    }
+));
+
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
--- server.js
+++ server.js
@@ -1,22 +1,107 @@
+// Eric Holguin
+// CSCI 3916: WEBAPI
+// Homework 2
+
+require('dotenv').config();
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var authController = require('./auth');
 
 var app = express();
-app.use(bodyParser.text({
-    type: function(req) {
-        return 'text';
-    }
-}));
 
-app.post('/post', function (req, res) {
-    console.log(req.body);
-    res = res.status(200);
-    if (req.get('Content-Type')) {
-        console.log("Content-Type: " + req.get('Content-Type'));
-        res = res.type(rq.get('Content-Type'));
+var uniqueKey = process.env.UNIQUE_KEY;
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+// Route /gets accepts get method
+router.route('/gets').get(function (req, res) {
+
+    var getInfo = {query: req.query, headers: req.headers, unique_key: uniqueKey}
+
+    if(Object.keys(req.query).length === 0)
+    {
+        res.status(400).send("No query");
     }
-    res.send(req.body);
+    if(Object.keys(req.headers).length === 0)
+    {
+        res.status(400).send("No headers");
+    }
+    else
+    {
+        res.status(200).send(getInfo);
+    }
+
 });
 
-http.createServer(app).listen(process.env.PORT || 8080);
+// Route /posts accepts get method
+router.route('/posts').post(function (req, res) {
+
+    var postInfo = {query: req.query, headers: req.headers, unique_key: uniqueKey}
+
+    if(Object.keys(req.query).length === 0)
+    {
+        res.status(400).send("No query");
+    }
+    if(Object.keys(req.headers).length === 0)
+    {
+        res.status(400).send("No headers");
+    }
+    else
+    {
+        res.status(200).send(postInfo);
+    }
+
+});
+
+
+// Route /puts accepts get method
+router.route('/puts').put(function (req, res) {
+
+    var putInfo = {query: req.query, headers: req.headers, unique_key: uniqueKey}
+
+    if(Object.keys(req.query).length === 0)
+    {
+        res.status(400).send("No query");
+    }
+    if(Object.keys(req.headers).length === 0)
+    {
+        res.status(400).send("No headers");
+    }
+    else
+    {
+        res.status(200).send(putInfo);
+    }
+
+});
+
+// Route /deletes accepts get method
+router.route('/deletes')
+    .delete(authController.isAuthenticated, function (req, res) {
+
+    var delInfo = {query: req.query, headers: req.headers, unique_key: uniqueKey}
+
+    if(Object.keys(req.query).length === 0)
+    {
+        res.status(400).send("No query");
+    }
+    if(Object.keys(req.headers).length === 0)
+    {
+        res.status(400).send("No headers");
+    }
+    else
+    {
+        res.status(200).send(delInfo);
+    }
+
+});
+
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
